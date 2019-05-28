ab66c8f8e7e514b8b754279ee1526432e8de1702
mecsantos <santos@wni.com>
2019-05-28 13:37:36 +0900

    Add connection logs

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var connectCounter = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
+  const sessionID = socket.id;
+  connectCounter++;
+
+  console.log(sessionID,' joined. Connections: ', connectCounter);
+
+  socket.on('disconnect', function(){
+    connectCounter--;
+    console.log(sessionID,' left. Connections: ', connectCounter);
+  });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
 });
 
 http.listen(port, function(){
